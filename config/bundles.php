2400f29c28e1119df8827f53951d465a3cdd0330
wyster <wyster@make.im>
2023-12-10 04:05:23 -0800

    add cors

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
