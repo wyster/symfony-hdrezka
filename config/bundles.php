54773d56dedd28fc141e8f8ef451c110e3a91647
wyster <wyster@make.im>
2023-12-10 03:08:56 -0800

    add sentry

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
