fa080739570c484c9b628b5916426762061afa37
Illia Zelenin <wyster@make.im>
2025-11-23 15:54:34 -0800

    load on dev only

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
 ];
